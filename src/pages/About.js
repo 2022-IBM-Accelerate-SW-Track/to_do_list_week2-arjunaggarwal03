b381126dec5487e3595b5d2ed018b23471728885
arjun aggarwal <arjun.2003.mail@gmail.com>
2022-06-15 19:11:48 -0400

    week one - about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
+import profile_pic from "../assets/me.png";
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
         <div class="split left">
           <div className="centered">
             <img
@@ -18,10 +17,11 @@ export default class About extends Component {
         </div>
         <div className="split right">
           <div className="centered">
-            <div className="name_title">Your Name</div>
+            <div className="name_title">Arjun Aggarwal</div>
             <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+              Hey! I'm from the <strong>DC area</strong> and I am a rising
+              sophomore at the <strong>University of Maryland</strong>. I'm
+              double majoring in CS and Math.
             </div>
           </div>
         </div>
